7e8c3d70505da0a27c9fd8d8e33de2a9bbd65ac5
M4r-ku5 <marcolocunsolo75@gmail.com>
2025-07-12 20:52:34 +0200

    Inizio package controller.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,4 @@
-import gui.HomeLogin;
-
-import javax.swing.*;
-
 public class Main {
-    
-    public static void main(String[] args) {
-
-        JFrame frame = new JFrame("Aeroporto di Napoli - Login");
-        HomeLogin loginForm = new HomeLogin();
-        frame.setContentPane(loginForm.getPanel());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-
-    }
-
+    public static void main(String[] args) {}
 }
 
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,34 +1,191 @@
 package controller;
 
-import java.util.ArrayList;
+import model.*;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.ArrayList;
+
+
+// ************************************************************************************
+//  ****************************** CLASSE CONTROLLER ***********************************
+//   ************************************************************************************
 
 public class Controller {
 
-    public String HandleLogin (String username, String password) {
+    /** Lista di utenti. */
+    private List<Utente> utenti;
 
-        if (username.equals("admin") && password.equals("admin")) {
-            return "ADMIN";
+    /** Lista di voli. */
+    private List<Volo> voli;
+
+    /** Lista di gates. */
+    private List<Gate> gates;
+
+    /** Lista di prenotazioni. */
+    private List<Prenotazione> prenotazioni;
+
+    /** Utente Loggato. */
+    private Utente utenteLoggato;
+
+
+
+
+    /**
+     * Costruttore della classe Controller.
+     */
+    public Controller() {
+        this.utenti = new ArrayList<>();
+        this.voli = new ArrayList<>();
+        this.gates = new ArrayList<>();
+        this.prenotazioni = new ArrayList<>();
+
+        this.utenti.add(new Amministratore("admin", "admin"));
+        this.utenti.add(new UtenteGenerico("user", "user"));
+
+        this.gates.add(new Gate((short)1));
+        this.gates.add(new Gate((short)2));
+        this.gates.add(new Gate((short)3));
+        this.gates.add(new Gate((short)4));
+    }
+
+
+
+
+    // ************************************************************************************
+    //  ************************************** METODI **************************************
+    //   ************************************************************************************
+
+    /**
+     * Metodo che gestisce il login dell'Utente.
+     * Il metodo scorre la lista di utenti e se trova una corrispondenza di login e password,
+     * effettua il login.
+     * @param login Login dell'Utente.
+     * @param password Password dell'Utente.
+     * @return true se il login va a buon fine, altrimenti false.
+     */
+    public boolean login(String login, String password) {
+        for (Utente u : this.utenti) {
+            if (u.getLogin().equals(login) && u.getPassword().equals(password)) {
+                this.utenteLoggato = u;
+                return true;
+            }
         }
-        else if (username.equals("user") && password.equals("user")) {
-            return "USER";
+        return false;
+    }
+
+
+    /**
+     * Metodo di logout dell'Utente.
+     * Il metodo setta a null l'Utente Loggato.
+     */
+    public void logout() {
+        this.utenteLoggato = null;
+    }
+
+
+    /**
+     * Metodo per verificare che l'Utente Loggato sia Amministratore.
+     * Il metodo verifica che utenteLoggato sia un'istanza di Amministratore.
+     * @return true se l'Utente Loggato è Amministratore, altrimenti false.
+     */
+    public boolean isAmministratore() {
+        return this.utenteLoggato instanceof Amministratore;
+    }
+
+
+    /**
+     * Metodo per aggiungere un Volo In Partenza.
+     * Il metodo controlla che l'Utente Loggato sia un Amministratore.
+     * Successivamente, aggiunge il volo.
+     * @param codiceVolo Codice del Volo.
+     * @param compagniaAerea Compagnia Aerea del Volo.
+     * @param dataVolo Data del Volo.
+     * @param orarioVolo Orario del Volo.
+     * @param aeroportoDestinazione Aeroporto di Destinazione del Volo in Partenza.
+     * @return true se l'operazione va a buon fine, altrimenti false.
+     */
+    public boolean aggiungiVoloInPartenza(String codiceVolo, String compagniaAerea, LocalDate dataVolo,
+                                          LocalTime orarioVolo, String aeroportoDestinazione) {
+
+        if (!(isAmministratore())) {
+            return false;
         }
+
         else {
-            return "FALLITO";
+            VoloInPartenza nuovoVolo = new VoloInPartenza(codiceVolo, compagniaAerea, dataVolo, orarioVolo,
+                                                            Duration.ZERO, aeroportoDestinazione);
+
+            this.voli.add(nuovoVolo);
+            return true;
         }
+
     }
 
 
-    public List<Volo> getAllVoli () {
-        List<Volo> voli = new ArrayList<>();
+    /**
+     * Metodo per aggiungere del Volo in Arrivo.
+     * @param codiceVolo Codice del Volo.
+     * @param compagniaAerea Compagnia Aerea del Volo.
+     * @param dataVolo Data del Volo.
+     * @param orarioVolo Orario del Volo.
+     * @param aeroportoOrigine Aeroporto di Origine del Volo.
+     * @return true se l'operazione va a buon fine, altrimenti false.
+     */
+    public boolean aggiungiVoloInArrivo(String codiceVolo, String compagniaAerea, LocalDate dataVolo,
+                                        LocalTime orarioVolo, String aeroportoOrigine) {
 
-        voli.add(new VoloInPartenza("AZ123", "Alitalia", "31/05/2025", "10:00", "0", StatoDelVolo.PROGRAMMATO, "Milano"));
-        voli.add(new VoloInArrivo("LH456", "Lufthansa", "31/05/2025", "15:00", "120", StatoDelVolo.IN_RITARDO, "Monaco"));
-        voli.add(new VoloInPartenza("RY789", "Ryanair", "01/06/2025", "15:00", "0", StatoDelVolo.CANCELATO, "Venezia"));
-        voli.add(new VoloInArrivo("BA101", "British Airways", "01/06/2025", "09:15", "0", StatoDelVolo.ATTERRATO, "Londra"));
+        if (!(isAmministratore())) {
+            return false;
+        }
 
-        return voli;
+        VoloInArrivo nuovoVolo = new VoloInArrivo(codiceVolo, compagniaAerea, dataVolo, orarioVolo,
+                Duration.ZERO, aeroportoOrigine);
+
+        this.voli.add(nuovoVolo);
+        return true;
     }
 
+
+    /**
+     * Metodo per assegnare un Gate a un Volo specifico.
+     * Il metodo controlla che l'Utente Loggato sia un Amministratore.
+     * Successivamente, controlla che il Volo e il Gate in ingresso esista effettivamente.
+     * Infine, se ciò è vero, setta il Gate al Volo.
+     * @param codiceVolo Codice del Volo.
+     * @param numeroGate Numero del Gate.
+     * @return true se l'operazione va a buon fine, altrimenti false.
+     */
+    public boolean assegnaGate (String codiceVolo, short numeroGate) {
+
+        if (!(isAmministratore())) {
+            return false;
+        }
+
+        Volo voloTrovato = null;
+        for (Volo v : this.voli) {
+            if (v.getCodiceVolo().equals(codiceVolo)) {
+                voloTrovato = v;
+                break;
+            }
+        }
+
+        Gate gateTrovato = null;
+        for (Gate g : this.gates) {
+            if (g.getNumeroGate() == numeroGate) {
+                gateTrovato = g;
+                break;
+            }
+        }
+
+        if (voloTrovato instanceof VoloInPartenza && gateTrovato != null) {
+            ((VoloInPartenza) voloTrovato).setGate(gateTrovato);
+            return true;
+        }
+
+        return false;
+    }
 
 }
